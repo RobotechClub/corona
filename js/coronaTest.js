0ed23c4c70050cd402de82d8b960b88c500b5150
Mahmoud <mfenej@dtp.ae>
2020-04-27 04:50:36 +0300

    fix styling issues on mobile

--- js/coronaTest.js
+++ js/coronaTest.js
@@ -156,13 +156,13 @@ function getMessage() {
         (isNo(1) && isYes(2) && isNo(3) && isYes(4)) ||
         (isNo(1) && isYesSeq(2, 3) && isNo(4)) ||
         (isYesSeq(1, 4) ||
-        (isYesSeq(1, 3) && isNo(4)) ||
-        (isYes(1) && isNo(2) && isYes(3) && isNo(4)) ||
-        (isYes(1) && isNo(2) && isYesSeq(3,4)) ||
-        (isYesSeq(1,2) && isNo(3) && isYes(4)))) {
+            (isYesSeq(1, 3) && isNo(4)) ||
+            (isYes(1) && isNo(2) && isYes(3) && isNo(4)) ||
+            (isYes(1) && isNo(2) && isYesSeq(3, 4)) ||
+            (isYesSeq(1, 2) && isNo(3) && isYes(4)))) {
         return messages[0].message;
     }
-    if (((isYesSeq(1, 2) && isNoSeq(3, 4)) || 
+    if (((isYesSeq(1, 2) && isNoSeq(3, 4)) ||
         (isYes(1) && isNoSeq(2, 4)) ||
         (isNo(1) && isYes(2) && isNoSeq(3, 4)))) {
         return messages[1].message;
@@ -182,7 +182,7 @@ function getMessage() {
 function stepNextAction() {
     $(".next").css("color", "black")
 
-    if(counter === 0){
+    if (counter === 0) {
         $("#ageSlider").hide();
         $("#yesNoSection").show();
         $("#counter").show();
@@ -194,7 +194,16 @@ function stepNextAction() {
 
     const value = $('input[name=choiceButon]:checked').val()
     if (!value && counter !== 0) {
-        swal('Please Choose Answer');
+        let message = ""
+        if (lang === "arabic") {
+            message = 'الرجاء اختيار الإجابة';
+        }
+        else if (message === "french") {
+            message = 'Veuillez choisir une réponse'
+        } else {
+            message = 'Please Choose Answer';
+        }
+        swal(message);
         return;
     }
 
@@ -235,7 +244,7 @@ function stepNextAction() {
         }
         else if (lang === "french") {
             $(".modal-body").css("direction", "ltr");
-            $("li").css({'direction':'ltr'});
+            $("li").css({ 'direction': 'ltr' });
             $("li").css("margin-left", "20px");
             $("#closeBtn").html("Fermer")
             $("#restTestBtn").html("Retester")
@@ -244,7 +253,7 @@ function stepNextAction() {
         }
         else {
             $(".modal-body").css("direction", "ltr");
-            $("li").css({'direction':'ltr'});
+            $("li").css({ 'direction': 'ltr' });
             $("li").css("margin-left", "20px");
             $("#closeBtn").html("Close")
             $("#restTestBtn").html("ReTest")
